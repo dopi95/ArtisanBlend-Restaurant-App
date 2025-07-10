20537a04bdc02c53380645fe203363d090daf39c
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-10 16:07:52 +0200

    Added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
+import Footer from "./components/Footer";
 export default function App() {
   return (
-    <h1 className="text-4xl font-bold text-center mt-20 text-blue-600">
-      React + Vite + Tailwind CSS is working!
-    </h1>
+    <Footer/>
   );
 }
