5d0c719aa5fd2bfbe55dff5cbdaad7c4428fd9bb
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-10 19:38:34 +0200

    Added a function to render all reviews dynamically

--- src/components/review.jsx
+++ src/components/review.jsx
@@ -1,10 +1,29 @@
 import reviewData from "../data/reviewsdata"
+import { useState } from "react"
+import { nanoid } from "nanoid";
 function Review(){
-    return (
-        <main className="grid place-content-center min-h-screen">
-            <article className="relative border border-solid border-black h-96 w-60 rounded-bl-[50px]">
-                <img src="images/image.png" className="absolute -top-5 -right-5 w-60"/>
-            </article>
+  const [reviews, setReviews] = useState(reviewData);
+
+  // render reviews
+  function renderReviews(){
+    return reviews.map((review)=>{
+      return(
+        <article key={nanoid()}>
+          <div className="relative h-96 w-60 rounded-bl-[3rem] border border-black">
+            <img src={`${review.image}`} className="w-60 absolute -top-4 -right-4"/>
+          </div>
+          <div></div>
+        </article>
+      )
+    })
+  }
+
+  return (
+        <main className="min-h-screen bg-slate-500 flex flex-col gap-10 p-5">
+          <h1 className="text-center text-4xl font-bold">Reviews</h1>
+          <section className="grid grid-cols-2 gap-8">
+            {renderReviews()}
+          </section>
         </main>
     )
 }
--- src/data/reviewsdata.js
+++ src/data/reviewsdata.js
@@ -1,4 +1,51 @@
 const reviewData = [
-  {image : "./review-images/review-image-1" }
+  {
+    image : "data/review-images/review-image-1.png",
+    comment: "Spacious seating, good natural light, and a touch of Nairobi’s urban charm. Love it!",
+    reviewer: "Khalifa",
+    rating: 5
+  },
+  {
+    image : "data/review-images/review-image-2.png",
+    comment: "What a gem! I stumbled upon Artisan Blend while exploring Nairobi, and it became my daily stop. Excellent service, a warm vibe, and honestly, better coffee than some places back home.",
+    reviewer: "Jonathan",
+    rating: 5
+  },
+  {
+    image : "data/review-images/review-image-3.png",
+    comment: "Their signature burger is juicy, well-seasoned, and served with crispy fries. A must-try!",
+    reviewer: "Finlay",
+    rating: 5
+  },
+  {
+    image : "data/review-images/review-image-4.png",
+    comment: "The avocado toast was fresh and flavorful — simple, but done perfectly!",
+    reviewer: "Elyas",
+    rating: 5
+  },
+  {
+    image : "data/review-images/review-image-5.png",
+    comment: "Warm lighting, soft jazz, and wooden decor — it feels like a cozy escape in the city." ,
+    reviewer: "Sena",
+    rating: 5
+  },
+  {
+    image : "data/review-images/review-image-6.png",
+    comment: "I wasn’t expecting much from the vegan bowl, but it blew me away. So satisfying!",
+    reviewer: "Yohanna",
+    rating: 5
+  },
+  {
+    image : "data/review-images/review-image-7.png",
+    comment: "Quiet enough to work, but lively enough to enjoy — the vibe strikes a perfect balance.",
+    reviewer: "Eric",
+    rating: 5
+  },
+  {
+    image : "data/review-images/review-image-8.png",
+    comment: "The indoor plants and earthy tones create a calm, nature-inspired atmosphere.",
+    reviewer: "Karaha",
+    rating: 5
+  },
 ];
 export default reviewData
