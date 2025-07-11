f3bf01de927fa404ec7463ce2fed70d7e28e4a95
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-11 12:42:15 +0200

    Made page responsive

--- src/components/LeaveReview.jsx
+++ src/components/LeaveReview.jsx
@@ -75,36 +75,36 @@ function LeaveReview(){
   return(
     <main className="p-5 md:max-h-screen">
       <h1 className="text-2xl font-bold">Leave a review</h1>
-      <section className="flex border-black border rounded-none mt-6 mx-4">
-        <article class="p-10 mx-8 flex flex-col gap-4 flex-1">
-          <h3 class="text-2xl">How would you rate us?</h3>
+      <section className="flex md:flex-row flex-col border-black border rounded-none mt-6 mx-4">
+        <article className="md:p-10 p-5 md:mx-8 mx-2 flex flex-col gap-4 flex-1">
+          <h3 className="text-2xl">How would you rate us?</h3>
           <StarRating/>
-          <form onSubmit={handleSubmit} class="md:max-w-3xl md:min-w-72 py-2">
-            <label for="name" class="text-gray-950 text-xl font-bold">Name:</label>
+          <form onSubmit={handleSubmit} className="md:max-w-3xl md:min-w-72 py-2">
+            <label htmlFor="name" className="text-gray-950 text-xl font-bold">Name:</label>
             <input 
               type="text" 
               id="name" 
               value={name} 
               onChange={(e)=> setName(e.target.value)} 
               placeholder="Name" 
-              class="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border-2 border-solid border-gray-700" 
+              className="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border-2 border-solid border-gray-700" 
               required
             />
             {nameError && <p className="text-xl font-bold text-orange-700">{nameError}</p>}
 
-            <label for="E-mail" class="text-gray-950 text-xl font-bold">E-mail:</label>
+            <label htmlFor="E-mail" className="text-gray-950 text-xl font-bold">E-mail:</label>
             <input 
               type="email" 
               id="E-mail" 
               placeholder="E-mail" 
               value={email} 
               onChange={(e)=> setEmail(e.target.value)} 
-              class="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border-2 border-solid border-gray-700" 
+              className="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border-2 border-solid border-gray-700" 
               required
             />
             {emailError && <p className="text-xl font-bold text-orange-700">{emailError}</p>}
 
-            <label for="review" class="text-gray-950 text-xl font-bold">Review:</label>
+            <label htmlFor="review" className="text-gray-950 text-xl font-bold">Review:</label>
             <textarea 
               name="review" 
               id="review" 
@@ -112,19 +112,19 @@ function LeaveReview(){
               onChange={(e)=> setReview(e.target.value)} 
               cols="30" rows="5" 
               placeholder="Review" 
-              class="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border-2 border-solid border-gray-700"
+              className="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border-2 border-solid border-gray-700"
             ></textarea>
             {reviewError && <p className="text-xl font-bold text-orange-700">{reviewError}</p>}
 
-            <div class="grid place-content-center w-full">
-              <button type="submit" class="px-4 py-2 bg-gold hover:bg-opacity-90 text-white font-bold rounded-lg">Review</button>
+            <div className="grid place-content-center w-full">
+              <button type="submit" className="px-4 py-2 bg-gold hover:bg-opacity-90 text-white font-bold rounded-lg">Review</button>
             </div>
 
             {submissionSuccess && <p className="text-xl text-green-500 font-bold">Form submitted successfully!</p>}
             {submissionError && <p className="text-xl text-red-500 font-bold">{submissionError}</p>}
           </form>
         </article>
-        <article className="w-1/3 bg-slate-600">
+        <article className="w-1/3 md:flex hidden  bg-slate-600">
           <img src="images/leavereview.png" className="md:h-full"/>
         </article>
       </section>
