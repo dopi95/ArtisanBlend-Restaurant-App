c86660c3d227fa5e9950ce3248435d5b185a7fa5
Elyas Yenealem <elyasyenealem@gmail.com>
2025-07-12 00:38:53 +0100

    update the home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from '../components/Hero';
 import SignatureDishes from '../components/SignatureDishes';
 import ChatBot from '../components/ChatBot';
 import About from '../components/About';
-import Review from '../components/Review'
+import Review from '../components/review'
 
 export default function Home() {
   return (
