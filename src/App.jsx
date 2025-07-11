eaac90164be41e3984b3a6b456a1e29bd491ec22
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-11 21:37:07 +0200

    Merging: Fixed react router issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import Footer from "./components/Footer";
-import About from "./components/About";
-import Review from "./components/review";
-import LeaveReview from "./components/LeaveReview";
+import Header from "./components/Header";
 import Reservation from "./components/reservation";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReservationData from "./components/ReservData";
@@ -9,18 +7,14 @@ import ReservationData from "./components/ReservData";
 export default function App() {
   return (
     <>
+      <Header/>
       <Router basename="/ArtisanBlend-Restaurant-App"> {/*basename to tell react that / means /ArtisanBlend-Restaurant-App */}
         <Routes>
           <Route path="/" element={<Reservation />} />
           <Route path="/reservation-successeded" element={<ReservationData />} />
-          {/*
-            <Route path="/about" element={<About />} />
-            <Route path="/reviews" element={<Review />} />
-            <Route path="/leave-review" element={<LeaveReview />} />
-            <Route path="/footer" element={<Footer />} />
-          */}
         </Routes>
       </Router>
+      <Footer/>
     </>
   );
 }
