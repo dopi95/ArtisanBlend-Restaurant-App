ae04fdb3d08e1ce05c3bb633337e5a16a7d2d63f
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-11 18:43:44 +0200

    Added form validation

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -1,31 +1,182 @@
+import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 function Reservation() {
+
+  // declare inpuut states
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [notes, setNotes] = useState("")
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [numGuests, setNumGuests] = useState(1);
+
+  // declare error states
+  const [firstNameError, setFirstNameError] = useState("");
+  const [secondNameError, setSecondNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+  const [dateError, setDateError] = useState("");
+  const [timeError, setTimeError] = useState("");
+
+  // state for submission feedback
+  const [submissionMessage, setSubmissionMessage] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // declare a variable to store char count of the notes input
+  const maxNotesCharacters = 50;
+
+  // form validation for specific inputs
+  // form validation logic
+  const validateForm = ()=>{ // returns a bollean
+    let isValid = true;
+
+    // name validation , name not null
+    if (!firstName.trim()){
+      setFirstNameError("Name is required");
+      isValid = false;
+    }else{
+      setFirstNameError("");
+    }
+
+    // name validation , name not null
+    if (!secondName.trim()){
+      setSecondNameError("Name is required");
+      isValid = false;
+    }else{
+      setSecondNameError("");
+    }
+
+    // email validation, not null and using regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email.trim()){
+      setEmailError("E-mail is required");
+      isValid = false;
+    }else{
+      setEmailError("");
+    }
+    // phone validation, not null and using regex
+    const phoneRegex = /^\+?(\d[\s.-]?){7,15}$/;
+    if (!phone.trim()){
+      setPhoneError("Phone is required");
+      isValid = false;
+    }else{
+      setPhoneError("");
+    }
+
+    // date validation
+    if (!selectedDate){
+      setDateError("Please select a date!");
+      isValid = false;
+    }else{
+      setDateError("")
+    }
+
+    // date validation
+    if (!selectedTime){
+      setTimeError("Please select a date!");
+      isValid = false;
+    }else{
+      setTimeError("");
+    }
+
+
+
+    // return if form is valid or not
+    return isValid
+  }
+
+  // implement form submission logic
+  const handleSubmit = (e)=>{
+    e.preventDefault();  // prevent default form submission behavious like reloading ...
+    setSubmissionMessage(""); //clear submission message if any
+    setIsSubmitted(false);
+
+    if(validateForm()){
+      //construct or make a reservation object, a new one
+      const newReservation = {
+        id: Date.now(), // simple id cz it returns millisecends as a number so we good
+        firstName,
+        secondName,
+        email,
+        phone,
+        notes,
+        date: selectedDate ? selectedDate.toISOString() : null,
+        time: selectedTime ? selectedTime.toTimeString().slice(0, 5) : null,
+        sumbmittedAt: new Date().toISOString(),
+      };
+
+      // get existing reservation if any from local storage and change them to objects
+      const existingReservationString = localStorage.getItem("reservations");
+      const existingReservation = existingReservationString ? JSON.parse(existingReservationString) : [];
+
+      // add new reservation to the array
+      const updatedReservations = [...existingReservation, newReservation];
+
+      // save the updated reservations to local storage
+      localStorage.setItem("reservations", JSON.stringify(updatedReservations));
+
+      // set sucess message
+      setSubmissionMessage("Your Place Booked, Thank you!")
+      setIsSubmitted(true);
+
+      // reset the fields
+      setFirstName("");
+      setSecondName("");
+      setEmail("");
+      setPhone("");
+      setNotes("");
+      setSelectedDate(null);
+      setSelectedTime(null);
+      setNumGuests(1);
+
+      //clear all errors
+      setFirstNameError("");
+      setEmailError("");
+      setSecondNameError("")
+      setPhoneError("");
+      setDateError("");
+      setTimeError("");
+    }else{
+      setSubmissionMessage("Please Correct errors in the Form!");
+      setIsSubmitted(false);
+    }
+  }
+
+
   return(
     <main className="px-5 flex flex-col items-center">
       <h1 className="text-center text-slate-950 font-bold text-4xl my-4">Make a reservation</h1>
       <section className="border rounded-tr-[50px] rounded-bl-[50px] border-gold border-solid w-2/3 py-4 px-6">
-        <form className="flex flex-col">
+        <form onSubmit={handleSubmit} className="flex flex-col">
           <div className="flex gap-4 ">
             <div className="flex-1">
               <label htmlFor="firstName" className="text-gray-950 text-xl font-semibold block">First Name:</label>
               <input 
                 type="text" 
                 id="firstName" 
-                placeholder="First Name" 
+                placeholder="John" 
                 className="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border border-solid border-gold" 
+                value={firstName}
+                onChange={(e)=>setFirstName(e.target.value)}
                 required
               />
+              {firstNameError && <p className="text-xl font-bold text-orange-700">{firstNameError}</p>}
             </div>
             <div className="flex-1">
               <label htmlFor="secondName" className="text-gray-950 text-xl font-semibold block">Second Name:</label>
               <input 
                 type="text" 
                 id="secondName" 
-                placeholder="Second Name" 
+                placeholder="Doe" 
                 className="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border border-solid border-gold" 
+                value={secondName}
+                onChange={(e)=>setSecondName(e.target.value)}
                 required
-            />
+              />
+              {secondNameError && <p className="text-xl font-bold text-orange-700">{secondNameError}</p>}
             </div>
           </div>
           <div className="flex gap-4 ">
@@ -34,29 +185,36 @@ function Reservation() {
               <input 
                 type="email" 
                 id="email" 
-                placeholder="E-mail" 
+                placeholder="johndoe@gmail.com" 
                 className="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border border-solid border-gold" 
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
                 required
               />
+              {emailError && <p className="text-xl font-bold text-orange-700">{emailError}</p>}
             </div>
             <div className="flex-1">
               <label htmlFor="tel" className="text-gray-950 text-xl font-semibold block">Phone Number:</label>
               <input 
                 type="tel" 
                 id="tel" 
-                placeholder="Phone Number" 
+                placeholder="+117-4536-5367" 
                 className="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border border-solid border-gold" 
+                value={phone}
+                onChange={(e)=>setPhone(e.target.value)}
                 required
-            />
+              />
+              {phoneError && <p className="text-xl font-bold text-orange-700">{phoneError}</p>}
             </div>
           </div>
           <div className="flex gap-4 ">
             <div className="w-max">
               <label htmlFor="guests" className="text-gray-950 text-xl font-semibold block">Number of Guests:</label>
               <select 
-                type="email" 
-                id="email" 
+                id="guests" 
                 className="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border border-solid border-gold" 
+                value={numGuests}
+                onChange={(e)=>setNumGuests(parseInt(e.target.value))}
                 required
               >
                 {[...Array(20)].map((_, i)=>(
@@ -64,14 +222,19 @@ function Reservation() {
                 ))}
               </select>
             </div>
-            <div className="flex-1">
-              <label htmlFor="notes" className="text-gray-950 text-xl font-semibold block">Notes(allergies,specific seats, duration, ocassion):</label>
-              <input 
-                type="text" 
-                id="notes" 
-                placeholder="Any Special Treatment" 
-                className="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border border-solid border-gold" 
-            />
+            <div className="flex-1 flex items-center gap-2">
+              <div className="flex-1">
+                <label htmlFor="notes" className="text-gray-950 text-xl font-semibold block">Notes(allergies,specific seats, duration, ocassion):</label>
+                <input 
+                  type="text" 
+                  id="notes" 
+                  placeholder="Any Special Treatment" 
+                  className="outline-none w-full rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border border-solid border-gold" 
+                  value={notes}
+                  onChange={(e)=>setNotes(e.target.value)}
+                />
+              </div>
+              <p className="border border-gold border-solid px-2 py-2 rounded-lg md:mt-4">{notes.length}/{maxNotesCharacters}</p>
             </div>
           </div>
           <div className="flex gap-4">
@@ -84,13 +247,17 @@ function Reservation() {
                 placeholderText="Select Reservation Date"
                 required
                 className="outline-none w-[25.5rem] rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border border-solid border-gold cursor-pointer"
+                selected={selectedDate}
+                onChange={(date)=> setSelectedDate(date)}
               />
+              {dateError && <p className="text-xl font-bold text-orange-700">{dateError}</p>}
             </div>
             <div className="flex-1">
               <label htmlFor="reservationTime" className="text-gray-950 text-xl font-semibold block">Reservation Time:</label>
               <DatePicker
                 id="reservationTime"
-                // selected={selectedTime}
+                selected={selectedTime}
+                onChange={(date)=> setSelectedTime(date)}
                 showTimeSelect
                 showTimeSelectOnly
                 dateFormat="h:mm aa"
@@ -100,12 +267,14 @@ function Reservation() {
                 required
                 className="outline-none w-[25.5rem] rounded-lg text-gray-950 text-xl px-2 py-2 mb-3 border border-solid border-gold cursor-pointer"
               />
+              {timeError && <p className="text-xl font-bold text-orange-700">{timeError}</p>}
             </div>
           </div>
           <div className="grid place-content-center w-full">
             <button type="submit" className="px-4 py-2 bg-darkgold hover:bg-opacity-90 text-white font-bold rounded-lg">Book</button>
           </div>
         </form>
+        {submissionMessage && <p className="text-xl text-green-500 font-bold">{submissionMessage}</p>}
       </section>
     </main>
   )
